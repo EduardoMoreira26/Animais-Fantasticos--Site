336f25a77d323891ed2141072a7b56b5cc80e05c
Eduardo <du_du.mba@hotmail.com>
2020-04-25 12:38:30 +0100

    animation faqList

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-//Criacção de navegação pelo click do usuário nas imagens.
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  //Criacção de navegação pelo click do usuário nas imagens.
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
-  //função que inclui a classe ativo nas secton e remove das outras
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+    //função que inclui a classe ativo nas section e remove das outras
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+    //evento para mudar class ativo nos itens pelo click
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
-  //evento para mudar class ativo nos itens pelo click
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
 }
+initTabNav();
+
+//Function que anima a FAQ list
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    //matendo sempre o primeiro elemento ativo
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    //adiciona ativo quando nao tem e remove quando tem
+    //do primeiro e no proximo elemento
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    //evento de click
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
