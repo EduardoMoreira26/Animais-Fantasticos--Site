6c68ac3ae507efb16f89eb67780f81eda01d0f9e
Eduardo <du_du.mba@hotmail.com>
2020-05-13 14:13:30 +0100

    js updates

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initiAnimationScroll from "./modules/anime-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
+
 initScrollSuave();
 initiAnimationScroll();
 initAccordion();
 initTabNav();
+initModal();
