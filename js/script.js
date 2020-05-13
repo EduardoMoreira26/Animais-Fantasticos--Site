78f336e421de8a270d19c67ddf052e4b7e3905fc
Eduardo <du_du.mba@hotmail.com>
2020-05-13 18:27:18 +0100

    js update

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ import initiAnimationScroll from "./modules/anime-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropDownMenu from "./modules/dropdown-menu.js";
 
 initScrollSuave();
 initiAnimationScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
+initDropDownMenu();
