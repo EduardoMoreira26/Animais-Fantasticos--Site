dbddd41ad13b4016c3e73eabf9f96a8e48efa439
Eduardo <du_du.mba@hotmail.com>
2020-05-14 19:21:48 +0100

    js menu-mobile

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,20 @@
+import outsideClick from "./outside-click.js"; //menu desaparece se clicar fora
+
+export default function initMenuMobile() {
+  const menuButton = document.querySelector("[data-menu='button']");
+  const menuList = document.querySelector("[data-menu='list']");
+  const eventos = ["click", "touchstart"];
+
+  if (menuButton) {
+    function openMenu(event) {
+      menuList.classList.add("ativo");
+      menuButton.classList.add("ativo");
+      outsideClick(menuList, eventos, () => {
+        menuList.classList.remove("ativo");
+        menuButton.classList.remove("ativo");
+      });
+    }
+
+    eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
+  }
+}
