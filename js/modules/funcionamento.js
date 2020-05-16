54d134a769be949da99c8d527625bf0f5e931b37
unknown <you@example.com>
2020-05-16 17:21:19 +0100

    updates js

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,20 @@
+//Function que anima a FAQ list
+export default function initFuncionamento() {
+
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+
+
+
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay()
+  const horarioAgora = dataAgora.getHours()
+
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1; //verifica o dia da semana se esta aberto
+  const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]); //verifica se o horario esta aberto
+
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto")
+  }
+}
